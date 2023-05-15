f219291201bd7c7b03a4ebbe840185fd14722666
NaBuchholz <nathaliabuchh@hotmail.com>
2023-05-15 15:12:30 -0300

    ✨ Paginate movies

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,14 @@
 import { CardMovie, Carrousel } from '../../components';
 import { useRouteLoaderData } from 'react-router';
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
-const HomePage = () => {
-	const movies = useRouteLoaderData('root');
-	console.log(movies);
+function HomePage({ currentItems }) {
 	return (
 		<div className="mb-16">
 			<Carrousel autoSlide={true}></Carrousel>
-			<div
-				className="flex flex-wrap gap-10 md:gap-16 justify-center 
-			items-start mt-16">
-				{movies.map((movie, index) => (
+			<div className="flex flex-wrap gap-10 md:gap-16 justify-center items-start mt-16">
+				{currentItems.map((movie, index) => (
 					<CardMovie
 						key={index}
 						movie={movie}
@@ -19,6 +17,36 @@ const HomePage = () => {
 			</div>
 		</div>
 	);
-};
+}
 
-export default HomePage;
+export default function PaginatedMovies() {
+	const movies = useRouteLoaderData('root');
+	const [itemOffset, setItemOffset] = useState(0);
+	const moviesPerPage = 10;
+	const endOffset = itemOffset + moviesPerPage;
+
+	const currentItems = movies.slice(itemOffset, endOffset);
+	const pageCount = Math.ceil(movies.length / moviesPerPage);
+
+	const handlePageClick = (event) => {
+		const newOffset = event.selected * moviesPerPage;
+
+		setItemOffset(newOffset);
+	};
+
+	return (
+		<>
+			<HomePage currentItems={currentItems} />
+			<ReactPaginate
+				breakLabel="..."
+				nextLabel=">"
+				onPageChange={handlePageClick}
+				pageRangeDisplayed={2}
+				pageCount={pageCount}
+				previousLabel="<"
+				renderOnZeroPageCount={null}
+				className="flex justify-center"
+			/>
+		</>
+	);
+}
